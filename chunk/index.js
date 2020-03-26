5e88d2665f5a92d11674e8baf2c92819c7d5a29a
Sergey Germanov <sergyleo@outlook.com>
2020-03-26 17:13:32 +1300

    arr chunks with While loop and splice

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,12 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  var arr = [];
+  while (array.length > 0) {
+    arr.push(array.splice(0, size));
+  }
+  return arr;
+}
 
 module.exports = chunk;
