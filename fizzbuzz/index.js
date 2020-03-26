f3c314b15a3a691be324bee947cdbf1cbd951ee9
Sergey Germanov <sergyleo@outlook.com>
2020-03-26 16:21:47 +1300

    classic solution with if and else if

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -13,15 +13,32 @@
 //   buzz
 
 function fizzBuzz(n) {
-  for (var i = 1; i <= n; i++) {
-    i % 5 === 0 && i % 3 === 0
-      ? console.log("fizzbuzz")
-      : i % 3 === 0
-      ? console.log("fizz")
-      : i % 5 === 0
-      ? console.log("buzz")
-      : console.log(i);
+  for (let i = 1; i <= n; i++) {
+    // Is the number a multiple of 3 and 5?
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      // Is the number a multiple of 3?
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
   }
 }
 
 module.exports = fizzBuzz;
+
+// ES6 if statements
+// function fizzBuzz(n) {
+//     for (var i = 1; i <= n; i++) {
+//       i % 5 === 0 && i % 3 === 0
+//         ? console.log("fizzbuzz")
+//         : i % 3 === 0
+//         ? console.log("fizz")
+//         : i % 5 === 0
+//         ? console.log("buzz")
+//         : console.log(i);
+//     }
+//   }
