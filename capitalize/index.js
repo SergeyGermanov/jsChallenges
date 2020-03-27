c61c737add5ac5625da1cf46a8f3230d48b2c8c3
Sergey Germanov <sergyleo@outlook.com>
2020-03-27 14:04:27 +1300

    capitalize solution slice

--- capitalize/index.js
+++ capitalize/index.js
@@ -8,15 +8,25 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  var strUp = str.split(" ");
-  var result = [];
-
-  for (let item of strUp) {
-    var letter = item[0].toUpperCase();
-    result.push(item.replace(item[0], letter));
+  const words = [];
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
   }
 
-  return result.join(" ");
+  return words.join(" ");
 }
 
 module.exports = capitalize;
+
+// split and for loop
+// function capitalize(str) {
+//     var strUp = str.split(" ");
+//     var result = [];
+
+//     for (let item of strUp) {
+//       var letter = item[0].toUpperCase();
+//       result.push(item.replace(item[0], letter));
+//     }
+
+//     return result.join(" ");
+//   }
