b7ec80380b912b6b424a5db7b543e684610de21d
Sergey Germanov <sergyleo@yahoo.com>
2020-04-02 18:40:22 +1300

    faster reccursion memoize solution

--- fib/index.js
+++ fib/index.js
@@ -8,14 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
   return fib(n - 1) + fib(n - 2);
 }
 
+const fib = memoize(slowFib);
+
 module.exports = fib;
+
+// slow reccursion solution
+// function fib(n) {
+//     if (n < 2) {
+//       return n;
+//     }
+//     return fib(n - 1) + fib(n - 2);
+//   }
 
 // for loop solution
 // function fib(n) {
